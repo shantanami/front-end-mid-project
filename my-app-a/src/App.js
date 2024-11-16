9228b1477b19009e280e352882f59b33ee66a8e5
shantanami <s22310550@student.unklab.ac.id>
2024-11-16 20:46:08 +0800

    no changes 4

--- my-app-a/src/App.js
+++ my-app-a/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import About from "/.components/About";
-import Contact from "/.components/Contact";
-import Footer from "/.components/Footer";
-import Header from "/.components/Header";
-import Home from "/.components/Home";
-import Works from "/.components/Works";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Works from "./components/Works";
 
 const App = () => {
   return (
--- my-app-a/src/components/About/index.js
+++ my-app-a/src/components/About/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const About = () => {
   return (
     <section id="about" className="s-about target-section">
